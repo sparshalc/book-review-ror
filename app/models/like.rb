e05a329a8484bc4b4e5478eb049bdec22d8186bd
sparsha3 <imailsparsha@gmail.com>
2023-01-31 15:35:51 +0545

    Added Validations to like

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,5 +2,5 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
-  validates :user_id, uniqueness: { scope: :book_id }
+  validates :user_id, uniqueness:{ scope: :book_id }
 end
